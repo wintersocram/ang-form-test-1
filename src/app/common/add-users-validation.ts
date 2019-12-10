b3880f9302c354bc2c3c7ffd238696f948ffaccf
Valois, Marcos (wintersocram) <valois.marcos@gmail.com>
2019-12-10 16:11:28 -0500

    reactive-form: correcting form validation checkPasswordMatch

--- src/app/common/add-users-validation.ts
+++ src/app/common/add-users-validation.ts
@@ -2,13 +2,13 @@ import { AbstractControl, ValidationErrors } from '@angular/forms';
 
 export class AddUsersValidator {
     static checkPasswordMatch(control: AbstractControl): ValidationErrors | null {
-        let password = control.get('password').value;
-        let confirmPassword = control.get('confirmPassword').value;
+        let password = control.get('password');
+        let confirmPassword = control.get('confirmPassword');
 
         if (!password.enabled && !confirmPassword.enabled)
             return null;
 
-        if (password === confirmPassword)
+        if (password.value === confirmPassword.value)
             return null;
 
         return { PasswordDoesNotMatch: true };
