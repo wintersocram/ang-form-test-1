1f865f8ea386066ed8bd467cf690d0e087e36ed1
Valois, Marcos (wintersocram) <valois.marcos@gmail.com>
2019-12-12 11:07:24 -0500

    router: initial route code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,62 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AddUserComponent } from './add-user/add-user.component';
+import { HomeComponent } from './home/home.component';
+import { MainUsersComponent } from './main-users/main-users.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+const appRoutes: Routes = [
+  { 
+    path: 'users/new-user', 
+    component: AddUserComponent, 
+    data: { title: 'Add new user' } 
+  },
+  { 
+    path: 'users/users-list/', 
+    component: UsersListComponent, 
+    data: { title: 'Users List' } 
+  },
+  { 
+    path: 'users/user-detail/:id', 
+    component: UserDetailComponent, 
+    data: { title: 'User' } 
+  },
+  { 
+    path: 'users', 
+    component: MainUsersComponent, 
+    data: { title: 'Users' } 
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: { title: 'Users Form' }, 
+    pathMatch: 'full' 
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUserComponent,
+    HomeComponent,
+    MainUsersComponent,
+    PageNotFoundComponent,
+    UserDetailComponent,
+    UsersListComponent,
+    NavBarComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
     BrowserModule
   ],
   providers: [],
---
+++ src/app/main-users/main-users.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'main-users',
+  templateUrl: './main-users.component.html',
+  styleUrls: ['./main-users.component.css']
+})
+export class MainUsersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
